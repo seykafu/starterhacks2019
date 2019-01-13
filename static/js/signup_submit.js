28e34d05b155222c3f3a2f2d1e0e00b7202e1828
seykafu <kaseykungfu@gmail.com>
2019-01-13 14:37:39 -0500

    wip

---
+++ static/js/signup_submit.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.signup submit').click(function() {
+
+        $.ajax({
+            url: '/signUp',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
