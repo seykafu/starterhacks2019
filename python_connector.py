358b6d345c2cd3bb3e15bf5260b8d16d65752939
seykafu <kaseykungfu@gmail.com>
2019-01-13 12:58:51 -0500

    wip

--- python_connector.py
+++ python_connector.py
@@ -1,4 +1,3 @@
-
 import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
@@ -7,16 +6,16 @@ try:
                              database='sh_questionaire',
                              user='seykafu',
                              password='398306014Yay!')
-   sql_insert_query = """ INSERT INTO `questionaire_data_new`
-                          (`email_user`, `cat1`, `cat2`, `cat3`, 'cat4', 'cat5', 'email_recipient')
-                          VALUES ('kaseykungfu@gmail.com', 'books', 'Camera, Photo & Video', 'Hats', 'Scarves', 'Socks','li.kh.joe@gmail.com')"""
+   sql_insert_query = """ INSERT INTO questionaire_data (email_user, cat1, cat2, cat3,
+   email_recipient) VALUES ('kaseykungfu@gmail.com', 'books', 'pets',
+   'Health & Personal Care', 'li.kh.joe@gmail.com'); """
    cursor = connection.cursor()
    result  = cursor.execute(sql_insert_query)
    connection.commit()
    print ("Record inserted successfully into questionaire_data_new table")
 except mysql.connector.Error as error :
     connection.rollback() #rollback if any exception occured
-    print("Failed inserting record into python_users table {}".format(error))
+    print("Failed inserting record into questionaire_data table {}".format(error))
 finally:
     #closing database connection.
     if(connection.is_connected()):
